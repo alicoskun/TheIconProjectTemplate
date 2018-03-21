92c5cab97c133750ec3f893d6aa2ca3a7ad3b921
Ali Coşkun <alicoskun@outlook.com>
2018-03-21 23:51:49 +0300

    added bootstrap

--- the-icon-project-template/src/App.js
+++ the-icon-project-template/src/App.js
@@ -1,49 +1,51 @@
 import React, { Component } from 'react';
 import './App.css';
-import './reset.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <div className="navbar navbar-inverse">
-            <div className="navbar-inner">
-              <div className="content-fluid">
-                <div className="nav-collapse collapse">
-                  <ul className="nav">
-                    <li className="navbar-item">
-                      <a href="#" title="Browse icons sets">Icon Sets</a>
-                    </li>
-                    <li className="navbar-item">
-                      <a href="#" title="Browse icon designers">Designers</a>
-                    </li>
-                    <li className="navbar-item">
-                      <a href="#" title="Custom icon design">Custom Icons</a>
-                    </li>
-                    <li className="navbar-item">
-                      <a href="#" title="Quickly edit icons">Icon Editor</a>
-                    </li>
-                    <li className="navbar-item">
-                      <a href="#" title="Subscribe">Pro</a>
-                    </li>
-                  </ul>
-                  <ul className="nav pull-right">
-                    <li className="navbar-item">
-                      <a href="#" title="Browse icons sets">Icon Sets</a>
-                    </li>
-                    <li className="navbar-item">
-                      <a href="#" title="Browse icon designers">Designers</a>
-                    </li>
-                    <li className="navbar-item">
-                      <a href="#" title="Custom icon design">Custom Icons</a>
-                    </li>
-                  </ul>
-                </div>
-              </div>
+        <nav className="navbar navbar-expand navbar-dark bg-dark">
+          <a tabIndex="0" className="navbar-brand">
+            <img src="https://getbootstrap.com/assets/brand/bootstrap-solid.svg" width="30" height="30" alt="" />
+          </a>
+          <div className="collapse navbar-collapse" id="navbarToggleExternalContent">
+            <ul className="navbar-nav">
+              <li className="nav-item active">
+                <a className="nav-link" href="#">Home <span className="sr-only">(current)</span></a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" href="#">Features</a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" href="#">Pricing</a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" href="#">Pro</a>
+              </li>
+            </ul>
+          </div>
+          <form className="form-inline">
+            <input className="form-control-sm mr-sm-2" type="search" placeholder="Search" aria-label="Search" />
+            <button className="btn btn-outline-info my-2 my-sm-0 btn-sm" type="submit">Search</button>
+          </form>
+          <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarToggleExternalContent" aria-controls="navbarToggleExternalContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span className="navbar-toggler-icon"></span>
+          </button>
+        </nav>
+        <div className="jumbotron jumbotron-fluid bg-dark">
+          <div className="container">
+            <h1 className="display-4 text-light">Fluid jumbotron</h1>
+            <p className="lead text-light">This is a modified jumbotron that occupies the entire horizontal space of its parent.</p>
+          </div>
+        </div>
+
+        <div className="container">
+          <div className="row justify-content-md-center">
+            <div className="col-md-10">
             </div>
           </div>
-        </header>
+        </div>
       </div>
     );
   }
