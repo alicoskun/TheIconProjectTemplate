cc6b35301eb11be73dad459ac81b4003c4e75f2b
Ali Coşkun <alicoskun@outlook.com>
2018-03-25 02:16:47 +0300

    changes on apperance

--- the-icon-project-template/src/App.js
+++ the-icon-project-template/src/App.js
@@ -1,52 +1,39 @@
 import React, { Component } from 'react';
+import Navbar from './components/Navbar';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <nav className="navbar navbar-expand navbar-dark bg-dark">
-          <a tabIndex="0" className="navbar-brand">
-            <img src="https://getbootstrap.com/assets/brand/bootstrap-solid.svg" width="30" height="30" alt="" />
-          </a>
-          <div className="collapse navbar-collapse" id="navbarToggleExternalContent">
-            <ul className="navbar-nav">
-              <li className="nav-item active">
-                <a className="nav-link" href="#">Home <span className="sr-only">(current)</span></a>
-              </li>
-              <li className="nav-item">
-                <a className="nav-link" href="#">Features</a>
-              </li>
-              <li className="nav-item">
-                <a className="nav-link" href="#">Pricing</a>
-              </li>
-              <li className="nav-item">
-                <a className="nav-link" href="#">Pro</a>
-              </li>
-            </ul>
-          </div>
-          <form className="form-inline">
-            <input className="form-control-sm mr-sm-2" type="search" placeholder="Search" aria-label="Search" />
-            <button className="btn btn-outline-info my-2 my-sm-0 btn-sm" type="submit">Search</button>
-          </form>
-          <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarToggleExternalContent" aria-controls="navbarToggleExternalContent" aria-expanded="false" aria-label="Toggle navigation">
-            <span className="navbar-toggler-icon"></span>
-          </button>
-        </nav>
+      <React.Fragment>
+        <Navbar />
         <div className="jumbotron jumbotron-fluid bg-dark">
           <div className="container">
-            <h1 className="display-4 text-light">Fluid jumbotron</h1>
-            <p className="lead text-light">This is a modified jumbotron that occupies the entire horizontal space of its parent.</p>
-          </div>
-        </div>
-
-        <div className="container">
-          <div className="row justify-content-md-center">
-            <div className="col-md-10">
+            <div className="title form-inline justify-content-center">
+              <img src="https://getbootstrap.com/assets/brand/bootstrap-solid.svg" width="70" height="70" alt="" />
+              <h1 className="display-4 text-light">ICONAREA</h1>
+            </div>
+            <div className="search-input d-flex justify-content-center">
+              <div className="input-group col-md-6">
+                <input className="form-control py-2  border-0" type="search" placeholder="Search for icons" aria-label="Search" />
+                <span className="input-group-append">
+                  <button className="btn btn-outline-secondary border-0" type="button">
+                    <i className="fa fa-search"></i>
+                  </button>
+                </span>
+              </div>
             </div>
           </div>
         </div>
-      </div>
+        <div className="container">
+          <div className="row justify-content-md-center">
+            <div className="col-md-10">
+              <div className="asd">
+              </div>
+            </div>
+          </div>
+        </div>
+      </React.Fragment>
     );
   }
 }
---
+++ the-icon-project-template/src/components/Navbar.js
@@ -0,0 +1,93 @@
+import React, { Component } from 'react';
+import person from '../assets/person.svg'
+import basket from '../assets/basket.svg'
+import envelope from '../assets/envelope.svg'
+import star from '../assets/star.svg'
+import bell from '../assets/bell.svg'
+
+class Navbar extends Component {
+  render() {
+    return (
+      <React.Fragment>
+        <nav className="navbar navbar-expand-md navbar-dark bg-dark">
+          <div className="navbar-brand">
+            <div className="form-inline">
+              <img src="https://getbootstrap.com/assets/brand/bootstrap-solid.svg" width="30" height="30" alt="" />
+              <h5 className="text-light">ICONAREA</h5>
+              <div className="nav-search search-input">
+                <div className="input-group">
+                  <input className="form-control py-2  border-0" type="search" placeholder="Search for icons" aria-label="Search" />
+                  <span className="input-group-append">
+                    <button className="btn btn-outline-secondary border-0" type="button">
+                      <i className="fa fa-search"></i>
+                    </button>
+                  </span>
+                </div>
+              </div>
+            </div>
+          </div>
+          <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarToggleContent" aria-controls="navbarToggleContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span className="navbar-toggler-icon"></span>
+          </button>
+          <div className="collapse navbar-collapse" id="navbarToggleContent">
+            <ul className="navbar-nav">
+              <li className="nav-item active">
+                <a className="nav-link" href="#">Home <span className="sr-only">(current)</span></a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" href="#">Features</a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" href="#">Pricing</a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" href="#">Pro</a>
+              </li>
+            </ul>
+            <ul className="navbar-nav ml-auto">
+              <li className="nav-item hidden-collapsed active">
+                <a className="nav-link" href="#">
+                  <img className="nav-image" src={star} width="20" height="20" alt="" />
+                </a>
+              </li>
+              <li className="nav-item hidden-collapsed">
+                <a className="nav-link" href="#">
+                  <img className="nav-image" src={envelope} width="20" height="20" alt="" />
+                </a>
+              </li>
+              <li className="nav-item hidden-collapsed">
+                <a className="nav-link" href="#">
+                  <img className="nav-image" src={bell} width="20" height="20" alt="" />
+                </a>
+              </li>
+              <li className="nav-item hidden-collapsed">
+                <a className="nav-link" href="#">
+                  <img className="nav-image" src={basket} width="20" height="20" alt="" />
+                </a>
+              </li>
+              <li className="nav-item hidden-collapsed">
+                <a className="nav-link" href="#">
+                  <img className="nav-image" src={person} width="20" height="20" alt="" />
+                </a>
+              </li>
+              <li className="nav-item show-collapsed">
+                <a className="nav-link" href="#">Messages</a>
+              </li>
+              <li className="nav-item show-collapsed">
+                <a className="nav-link" href="#">Notifications</a>
+              </li>
+              <li className="nav-item show-collapsed">
+                <a className="nav-link" href="#">Cart</a>
+              </li>
+              <li className="nav-item show-collapsed">
+                <a className="nav-link" href="#">Profile</a>
+              </li>
+            </ul>
+          </div>
+        </nav>
+      </React.Fragment>
+    );
+  }
+}
+
+export default Navbar;
