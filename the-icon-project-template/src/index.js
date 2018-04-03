79d48f8603e32b25a7eb9e3f13d810092a0e8586
Ali Coşkun <alicoskun@outlook.com>
2018-04-04 00:07:49 +0300

    react routing

--- the-icon-project-template/src/index.js
+++ the-icon-project-template/src/index.js
@@ -5,7 +5,7 @@ import './index.css';
 import App from './App';
 
 ReactDOM.render(
-    <BrowserRouter>
+    <BrowserRouter basename={window.location.pathname}>
         <App />
     </BrowserRouter>, 
     document.getElementById('root')
